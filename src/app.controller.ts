0bbe94942fe68bcb5871e95a0ba2dde083d9489a
oat <athiwat@sellsuki.com>
2021-07-12 11:42:11 +0700

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, UseGuards, HttpStatus, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
+import { Request } from 'express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/facebook')
+  @UseGuards(AuthGuard('facebook'))
+  async facebookLogin(): Promise<any> {
+    return HttpStatus.OK;
+  }
+
+  @Get('/facebook/redirect')
+  @UseGuards(AuthGuard('facebook'))
+  async facebookLoginRedirect(@Req() req: Request): Promise<any> {
+    return {
+      statusCode: HttpStatus.OK,
+      data: req.user,
+    };
+  }
+}
---
+++ src/facebook.strategy.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-facebook';
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
+  constructor() {
+    super({
+      clientID: '194416942616139',
+      clientSecret: '552a400cf8df6b5af40412ba80ccf95e',
+      callbackURL: 'http://192.168.1.35:3002/facebook/redirect',
+      scope: 'email',
+      display: 'popup',
+      profileFields: ['emails', 'name'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void,
+  ): Promise<any> {
+    const { name, emails } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+    };
+    const payload = {
+      user,
+      accessToken,
+    };
+
+    done(null, payload);
+  }
+}
